1f8bf505a61532dffad5d11e8b80450e7690d232
AyubElmiSilver <09ayub10@gmail.com>
2023-09-16 12:36:12 +0100

    Add conditional statemets
    This will be done using if else chain that will determine winner message

--- game.js
+++ game.js
@@ -1,8 +1,35 @@
 //Declare a function
-//Use Math.random and Math.floor to generate a random item from the array list
+//Use Math.random and Math.floor to generate a random item from the array list which will be the computer choice
 
 let getComputerChoice = function() {
     const choose = ['Rock', 'Paper', 'Scissors'] 
     return choose[Math.floor(Math.random() * choose.length)]
 } 
-console.log(getComputerChoice())
+
+//Declare a function that takes two parameters
+//Create conditional statements depending on combinations given by player and computer and ensure that input is case insensitive
+//Output a string message that declares the winner based on the combination.
+//Run your code using the players selection and computer choice as arguments 
+ 
+function computerAgainstPlayer (playerSelection, computerSelection) {
+    if (playerSelection == null) {
+        return ('Try again. Enter Rock, Paper or Scissors'); 
+    }  else if (playerSelection == computerSelection) {
+        return (`Draw! You both selected ${playerSelection}`);
+    } else if (playerSelection.toUpperCase() == 'ROCK' & computerSelection == 'Scissors') {
+        return ('You Win! Rock beats Scissors'); 
+    } else if (playerSelection.toUpperCase() == 'ROCK' & computerSelection == 'Paper') {
+        return ('You lose! Paper beats Rock');
+    } else if (playerSelection.toUpperCase() == 'PAPER' & computerSelection == 'Rock') {
+        return ('You Win! Paper beats Rock'); 
+    } else if (playerSelection.toUpperCase() == 'PAPER' & computerSelection == 'Scissors') {
+        return ('You Lose! Scissors beats Rock'); 
+    } else if (playerSelection.toUpperCase() == 'SCISSORS' & computerSelection == 'Paper') {
+        return ('You Win! Scissors beats Paper'); 
+    } else if (playerSelection.toUpperCase() == 'SCISSORS' & computerSelection == 'Rock') {
+        return ('You Lose! Rock beats Scissors '); 
+    } else {
+        return ('Try again. Enter Rock, Paper or Scissors');
+    } 
+}
+console.log(computerAgainstPlayer(prompt('Enter Rock or Paper or Scissors'), getComputerChoice()));

0ea7d0d35c88944e87cb9d0ae86295d82e5df67c
AyubElmiSilver <09ayub10@gmail.com>
2023-09-17 19:33:13 +0100

    Add uppercase to parameters for draw comparison.
    This is so that the draw comparison works as intended.

--- game.js
+++ game.js
@@ -11,25 +11,28 @@ let getComputerChoice = function() {
 //Output a string message that declares the winner based on the combination.
 //Run your code using the players selection and computer choice as arguments 
  
-function computerAgainstPlayer (playerSelection, computerSelection) {
+function computerAgainstPlayer(playerSelection, computerSelection) {
     if (playerSelection == null) {
         return ('Try again. Enter Rock, Paper or Scissors'); 
-    }  else if (playerSelection == computerSelection) {
+    //convert both comparisons to uppercase for comparison to work
+    }  else if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
         return (`Draw! You both selected ${playerSelection}`);
-    } else if (playerSelection.toUpperCase() == 'ROCK' & computerSelection == 'Scissors') {
+    } else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'Scissors') {
         return ('You Win! Rock beats Scissors'); 
-    } else if (playerSelection.toUpperCase() == 'ROCK' & computerSelection == 'Paper') {
-        return ('You lose! Paper beats Rock');
-    } else if (playerSelection.toUpperCase() == 'PAPER' & computerSelection == 'Rock') {
+    } else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'Paper') {
+        return ('You lose! Paper beats Rock'); 
+    } else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'Rock') {
         return ('You Win! Paper beats Rock'); 
-    } else if (playerSelection.toUpperCase() == 'PAPER' & computerSelection == 'Scissors') {
+    } else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'Scissors') {
         return ('You Lose! Scissors beats Rock'); 
-    } else if (playerSelection.toUpperCase() == 'SCISSORS' & computerSelection == 'Paper') {
+    } else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'Paper') {
         return ('You Win! Scissors beats Paper'); 
-    } else if (playerSelection.toUpperCase() == 'SCISSORS' & computerSelection == 'Rock') {
+    } else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'Rock') {
         return ('You Lose! Rock beats Scissors '); 
     } else {
         return ('Try again. Enter Rock, Paper or Scissors');
     } 
 }
+
+
 console.log(computerAgainstPlayer(prompt('Enter Rock or Paper or Scissors'), getComputerChoice()));
